793cc6ff26f8c537481cd8a629c44ce73228d279
jianyestudy <1226642@qq.com>
2022-01-18 09:59:23 +0800

    ././.

--- src/core/router.js
+++ src/core/router.js
@@ -1,11 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-    { path: '/convert-user', component: () => import("../components/convertUser/convertUser.vue")},
-    { path: '/query-play', component: () => import("../components/queryPlay/queryPlay.vue")},
+    {
+        path: '/convert-user',
+        component: () => import("../components/convertUser/convertUser.vue"),
+        meta: {
+            name: '用户名转化'
+        }
+    },
+    {
+        path: '/query-play',
+        component: () => import("../components/queryPlay/queryPlay.vue"),
+        meta: {
+            name: '播放列表查询'
+        }
+    },
     {
         path: '/',
         component: () => import("../components/index/index.vue"),
+        meta: {
+            name: 'tiktok工具',
+        }
     },
 ]
 const router = createRouter({
--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import './assets/reset.css'
 import App from './App.vue'
 import router from './core/router'
 
+router.beforeEach((to, from, next) => {
+    /* 路由发生变化修改页面title */
+    if (to.meta.name) {
+        document.title = to.meta.name
+    }
+    next()
+})
 
 const app = createApp(App)
 app.use(ElementPlus)
